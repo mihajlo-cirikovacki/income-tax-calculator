646735b3bfc9c9ddc9686d0367f9b314ec9a8a2e
mihajlo <mihajlo.cirikovacki@quantox.com>
2023-02-06 18:04:57 +0100

    .

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,7 +5,7 @@ import { useForm } from 'react-hook-form';
 import { TaxRateInput } from './TaxRateInput';
 import { TotalInput } from './TotalInput';
 import { useIncomeContext } from '@/context/IncomeContext';
-import { Button, GrossNetPicker } from '@/components/Elements';
+import { Button, GrossNetPicker } from '../Elements';
 
 export const Form = () => {
 	const {
